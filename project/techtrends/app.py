0b5c809c04863e8e89ca847d59bb5dd6fc38312e
Abdullah-Ashehry <abboodfs@gmail.com>
2024-10-16 21:39:25 +0300

    feat:

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -4,12 +4,36 @@ from flask import Flask, jsonify, json, render_template, request, url_for, redir
 from werkzeug.exceptions import abort
 import logging
 
+# Define the Flask application
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your secret key'
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+file_handler = logging.FileHandler('app.log')  # Log to file
+file_handler.setLevel(logging.DEBUG)
+
+console_handler = logging.StreamHandler()  # Log to console (stdout)
+console_handler.setLevel(logging.DEBUG)
+
+# Create a formatter and set it for both handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
+db_connection_counter = 0 
 def get_db_connection():
+    global db_connection_counter  # Declare the global variable
+    db_connection_counter += 1  # Increment the connection counter
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    app.logger.debug('retuning DB connection')
     return connection
 
 # Function to get a post using its ID
@@ -18,11 +42,9 @@ def get_post(post_id):
     post = connection.execute('SELECT * FROM posts WHERE id = ?',
                         (post_id,)).fetchone()
     connection.close()
+    app.logger.debug('getting post id', post_id)
     return post
 
-# Define the Flask application
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your secret key'
 
 # Define the main route of the web application 
 @app.route('/')
@@ -30,6 +52,7 @@ def index():
     connection = get_db_connection()
     posts = connection.execute('SELECT * FROM posts').fetchall()
     connection.close()
+    app.logger.debug('getting all the posts')
     return render_template('index.html', posts=posts)
 
 # Define how each individual article is rendered 
@@ -38,9 +61,11 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
-      return render_template('404.html'), 404
+        app.logger.debug('no posts available')
+        return render_template('404.html'), 404
     else:
-      return render_template('post.html', post=post)
+        app.logger.debug('returning post')
+        return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
@@ -55,6 +80,7 @@ def create():
         content = request.form['content']
 
         if not title:
+            app.logger.debug('No title provided')
             flash('Title is required!')
         else:
             connection = get_db_connection()
@@ -62,9 +88,11 @@ def create():
                          (title, content))
             connection.commit()
             connection.close()
+            app.logger.debug('record inserted')
 
             return redirect(url_for('index'))
-
+    app.logger.debug('returning create.html')
+    
     return render_template('create.html')
 
 @app.route('/healthz')
@@ -74,22 +102,33 @@ def healthcheck():
             status=200,
             mimetype='application/json'
     )
-    app.logger.info('Status request successfull')
-    app.logger.debug('DEBUG message')
+    app.logger.debug('Status request successfull')
     return response
 
 @app.route('/metrics')
 def metrics():
+    connection = get_db_connection()
+    total_posts = connection.execute('SELECT COUNT(*) FROM posts').fetchone()[0]  # Get total number of posts
+    connection.close()
+    
     response = app.response_class(
-            response=json.dumps({"status":"success","code":0,"data":{"UserCount":140,"UserCountActive":23}}),
-            status=200,
-            mimetype='application/json'
+        response=json.dumps({
+            "status": "success",
+            "code": 0,
+            "data": {
+                "TotalPosts": total_posts,
+                "TotalConnections": db_connection_counter  # Return total number of database connections
+            }
+        }),
+        status=200,
+        mimetype='application/json'
     )
-    app.logger.info('Metrics request successfull')
+    app.logger.debug('Metrics request successful')
     return response
+
 # start the application on port 3111
 if __name__ == "__main__":
     ## stream logs to a file
-    logging.basicConfig(filename='app.log',level=logging.DEBUG)
+    # logging.basicConfig(filename='app.log', level=logging.DEBUG, force=True)
     
     app.run(host='0.0.0.0', port='3111')
